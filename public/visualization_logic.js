ae04c8d0ffdbbb210ce7209e08ee1e19ac4470db
Jack Meyers <jackmeyers3@gmail.com>
2022-07-30 13:24:06 -0700

    annotating

--- public/visualization_logic.js
+++ public/visualization_logic.js
@@ -30,9 +30,9 @@ function updateSlide() {
     }
 }
 
-function showCheckInVis(){
+function showCheckInVis() {
     updateGraphTitle("Checked-in Border Crossings");
-    updateGraphSubtext("");
+    updateGraphSubtext("Checked-in border crossings denote a standard border crossing with a visa.");
     createCheckInGraph(getDirectionalCheckInData(global_data, isToPoland()));
     showDirectionToggle();
     hideCrossingType();
@@ -50,7 +50,7 @@ function showEvacuationVis() {
 
 function showNetCrossingVis() {
     updateGraphTitle("Net Border Crossings from Ukraine to Poland");
-    updateGraphSubtext("");
+    updateGraphSubtext("Negative values denote a positive flow from Poland to Ukraine.");
     hideDirectionToggle();
     showCrossingType();
     createNetGraph(getNetCrossingData(global_data));
@@ -60,7 +60,7 @@ function showNetCrossingVis() {
 function getNextSlide() {
     let index = (getCurrentSlideIndex() + 1) % 3;
     document.getElementById("visualization").setAttribute("data-slide", index);
-    document.getElementById("slide-number").innerHTML = index+1;
+    document.getElementById("slide-number").innerHTML = index + 1;
     updateSlide();
 }
 
@@ -68,31 +68,9 @@ function getNextSlide() {
     Graph Logic
 ****************************************************************/
 
-function createEvacuationGraph(data) {
-    clearGraph();
-    var parseDate = d3.timeParse("%Y-%m-%d");
-    var mindate = parseDate("2022-01-01"),
-        maxdate = parseDate("2022-03-31");
-
-    var x = d3.scaleLinear().domain([0, 90]).range([0, 500]);
-    var y = d3.scaleLinear().domain([0, 100000]).range([500, 0]);
-    var height = d3.scaleLinear().domain([0, 100000]).range([0, 500]);
-    var xDate = d3.scaleTime().domain([mindate, maxdate]).range([0, 500]);
-
-    d3.select("svg").append("g").attr("transform","translate(50,50)")
-        .selectAll("rect").data(data).enter().append("rect")
-        .attr("x", function(d,i) { return x(i); })
-        .attr("y", function(d) { return y(d.Count); })
-        .attr("width", "4.5")
-        .attr("height", function(d) {return height(d.Count);});
-
-    d3.select("svg").append("g").attr("transform", "translate(50,50)").call(d3.axisLeft(y).ticks(7).tickFormat(d3.format("d"))).append("text")
-    .attr("fill", "#000").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", "0.8em").attr("text-anchor", "end").text("Total daily count of people");
-    d3.select("svg").append("g").attr("transform", "translate(50,550)").call(d3.axisBottom(xDate).tickFormat(d3.timeFormat("%b-%d")).tickValues(sampleDates(data).map(function (d) { return new Date(d.Date) })))
-}
-
 function createCheckInGraph(data) {
     clearGraph();
+    let svg = d3.select("svg");
     var parseDate = d3.timeParse("%Y-%m-%d");
     var mindate = parseDate("2022-01-01"),
         maxdate = parseDate("2022-03-31");
@@ -102,20 +80,129 @@ function createCheckInGraph(data) {
     var height = d3.scaleLinear().domain([0, 150000]).range([0, 500]);
     var xDate = d3.scaleTime().domain([mindate, maxdate]).range([0, 500]);
 
-    d3.select("svg").append("g").attr("transform","translate(50,50)")
+    svg.append("g").attr("transform", "translate(50,50)")
         .selectAll("rect").data(data).enter().append("rect")
-        .attr("x", function(d,i) { return x(i); })
-        .attr("y", function(d) { return y(d.Count); })
+        .attr("x", function (d, i) { return x(i); })
+        .attr("y", function (d) { return y(d.Count); })
         .attr("width", "4.5")
-        .attr("height", function(d) {return height(d.Count);});
+        .attr("height", function (d) { return height(d.Count); });
 
-    d3.select("svg").append("g").attr("transform", "translate(50,50)").call(d3.axisLeft(y).ticks(7).tickFormat(d3.format("d"))).append("text")
-    .attr("fill", "#000").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", "0.8em").attr("text-anchor", "end").text("Total daily count of people");
-    d3.select("svg").append("g").attr("transform", "translate(50,550)").call(d3.axisBottom(xDate).tickFormat(d3.timeFormat("%b-%d")).tickValues(sampleDates(data).map(function (d) { return new Date(d.Date) })))
+    svg.append("g").attr("transform", "translate(50,50)").call(d3.axisLeft(y).ticks(7).tickFormat(d3.format("d"))).append("text")
+        .attr("fill", "#000").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", "0.8em").attr("text-anchor", "end").text("Total daily count of people");
+    svg.append("g").attr("transform", "translate(50,550)").call(d3.axisBottom(xDate).tickFormat(d3.timeFormat("%b-%d")).tickValues(sampleDates(data).map(function (d) { return new Date(d.Date) })))
+
+    addCheckInAnnotations(x, y);
+}
+
+function addCheckInAnnotations(x, y) {
+    let annotations = []
+    if (isToPoland()) {
+        annotations = [
+            {
+                note: {
+                    label: "Avg daily count: 16,873 persons",
+                    title: "Jan-21 : Feb-21",
+                    wrap: 150,
+                },
+                x: x(21),
+                y: y(20000),
+                dy: -90,
+                dx: 20,
+                subject: { width: 175, height: 50 },
+                type: d3.annotationCalloutRect,
+            },
+            {
+                note: {
+                    label: "Avg daily count: 97,000 persons",
+                    title: "Feb-26 (2 days after invasion): Mar-04 ",
+                    wrap: 270,
+                },
+                x: x(56),
+                y: y(100000),
+                dy: 20,
+                dx: -40,
+                subject: { width: 40, height: 100 },
+                type: d3.annotationCalloutRect,
+            },
+        ]
+    }
+    else {
+        annotations = [
+            {
+                note: {
+                    label: "Beginning of Russian Invasion",
+                    title: "February 24th",
+                    wrap: 200,
+                },
+                x: x(55),
+                y: y(12500),
+                dy: -50,
+                dx: -40,
+                type: d3.annotationCalloutElbow,
+            },
+            {
+                note: {
+                    label: "Little change in flow of people to Ukraine",
+                    title: "Weeks following invasion",
+                    wrap: 280,
+                },
+                x: x(60),
+                y: y(18000),
+                dy: -100,
+                dx: -10,
+                subject: { width: 100, height: 50 },
+                type: d3.annotationCalloutRect,
+            },
+        ]
+    }
+    d3.select("svg").append("g").attr("transform", "translate(50,50)").call(d3.annotation().annotations(annotations))
+    return;
+}
+
+function createEvacuationGraph(data) {
+    clearGraph();
+    let svg = d3.select("svg");
+    var parseDate = d3.timeParse("%Y-%m-%d");
+    var mindate = parseDate("2022-01-01"),
+        maxdate = parseDate("2022-03-31");
+
+    var x = d3.scaleLinear().domain([0, 90]).range([0, 500]);
+    var y = d3.scaleLinear().domain([0, 100000]).range([500, 0]);
+    var height = d3.scaleLinear().domain([0, 100000]).range([0, 500]);
+    var xDate = d3.scaleTime().domain([mindate, maxdate]).range([0, 500]);
+
+    svg.append("g").attr("transform", "translate(50,50)")
+        .selectAll("rect").data(data).enter().append("rect")
+        .attr("x", function (d, i) { return x(i); })
+        .attr("y", function (d) { return y(d.Count); })
+        .attr("width", "4.5")
+        .attr("height", function (d) { return height(d.Count); });
+
+    svg.append("g").attr("transform", "translate(50,50)").call(d3.axisLeft(y).ticks(7).tickFormat(d3.format("d"))).append("text")
+        .attr("fill", "#000").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", "0.8em").attr("text-anchor", "end").text("Total daily count of people");
+    svg.append("g").attr("transform", "translate(50,550)").call(d3.axisBottom(xDate).tickFormat(d3.timeFormat("%b-%d")).tickValues(sampleDates(data).map(function (d) { return new Date(d.Date) })))
+
+    const annotations = [
+        {
+            note: {
+                label: "Beginning of Russian Invasion",
+                title: "February 24th",
+                wrap: 150,
+            },
+            x: x(53),
+            y: y(5000),
+            dy: -50,
+            dx: -50,
+            type: d3.annotationCalloutElbow,
+        },
+    ]
+    svg.append("g").attr("transform", "translate(50,50)").call(d3.annotation().annotations(annotations))
 }
 
 function createNetGraph(data) {
     clearGraph();
+    let svg = d3.select("svg");
+
     var parseDate = d3.timeParse("%Y-%m-%d");
     var mindate = parseDate("2022-01-01"),
         maxdate = parseDate("2022-03-31");
@@ -125,16 +212,16 @@ function createNetGraph(data) {
     var height = d3.scaleLinear().domain([0, 250000]).range([0, 416.667]);
     var xDate = d3.scaleTime().domain([mindate, maxdate]).range([0, 500]);
 
-    d3.select("svg").append("g").attr("transform","translate(50,50)")
+    svg.append("g").attr("transform", "translate(50,50)")
         .selectAll("rect").data(data).enter().append("rect")
-        .attr("x", function(d,i) { return x(i); })
-        .attr("y", function(d) { return d.Count < 0 ? 416.667 : y(d.Count) ; })
+        .attr("x", function (d, i) { return x(i); })
+        .attr("y", function (d) { return d.Count < 0 ? 416.667 : y(d.Count); })
         .attr("width", "4.5")
-        .attr("height", function(d) {return d.Count < 0 ? height(-d.Count): height(d.Count); });
+        .attr("height", function (d) { return d.Count < 0 ? height(-d.Count) : height(d.Count); });
 
-    d3.select("svg").append("g").attr("transform", "translate(50,50)").call(d3.axisLeft(y).ticks(7).tickFormat(d3.format("d"))).append("text")
-    .attr("fill", "#000").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", "0.8em").attr("text-anchor", "end").text("Total daily count of people");
-    d3.select("svg").append("g").attr("transform", "translate(50,550)").call(d3.axisBottom(xDate).tickFormat(d3.timeFormat("%b-%d")).tickValues(sampleDates(data).map(function (d) { return new Date(d.Date) })))
+    svg.append("g").attr("transform", "translate(50,50)").call(d3.axisLeft(y).ticks(7).tickFormat(d3.format("d"))).append("text")
+        .attr("fill", "#000").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", "0.8em").attr("text-anchor", "end").text("Total daily count of people");
+    svg.append("g").attr("transform", "translate(50,550)").call(d3.axisBottom(xDate).tickFormat(d3.timeFormat("%b-%d")).tickValues(sampleDates(data).map(function (d) { return new Date(d.Date) })))
 }
 
 function clearGraph() {
@@ -195,7 +282,7 @@ function getDirectionalEvacuationData(data, toPoland) {
 
 function getNetCrossingData(data) {
     let netCrossings = []
-    switch(getCrossingType()){
+    switch (getCrossingType()) {
         case "all":
             netCrossings = sumCountArrays(getNetCheckedInData(data), getNetEvacuationData(data))
             break;
@@ -210,14 +297,14 @@ function getNetCrossingData(data) {
 }
 
 //Returns net crossings with respect to Poland - Ukraine
-function getNetCheckedInData(data){
+function getNetCheckedInData(data) {
     let checkedInToPoland = getDirectionalCheckInData(data, true);
     let checkedInToUkraine = getDirectionalCheckInData(data, false);
     return subtractCountArrays(checkedInToPoland, checkedInToUkraine);
 }
 
 //Returns net crossings with respect to Poland - Ukraine
-function getNetEvacuationData(data){
+function getNetEvacuationData(data) {
     let evacuatedToPoland = getDirectionalEvacuationData(data, true);
     let evacuatedToUkraine = getDirectionalEvacuationData(data, false);
     return subtractCountArrays(evacuatedToPoland, evacuatedToUkraine);
@@ -227,19 +314,19 @@ function getNetEvacuationData(data){
     Utilities / Tests
 ****************************************************************/
 
-function sumCountArrays(arr1, arr2){
+function sumCountArrays(arr1, arr2) {
     let output = [];
-    for (let i = 0; i < arr1.length; i++){
-        let entry = {Date: arr1[i].Date, Count: arr1[i].Count + arr2[i].Count}
+    for (let i = 0; i < arr1.length; i++) {
+        let entry = { Date: arr1[i].Date, Count: arr1[i].Count + arr2[i].Count }
         output.push(entry);
     }
     return output;
 }
 
-function subtractCountArrays(arr1, arr2){
+function subtractCountArrays(arr1, arr2) {
     let output = [];
-    for (let i = 0; i < arr1.length; i++){
-        let entry = {Date: arr1[i].Date, Count: arr1[i].Count - arr2[i].Count}
+    for (let i = 0; i < arr1.length; i++) {
+        let entry = { Date: arr1[i].Date, Count: arr1[i].Count - arr2[i].Count }
         output.push(entry);
     }
     return output;
